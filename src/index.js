f32264d2c8f8b87b87c037f37f563a112b1c41d6
Daniil Stasulevich <alexgrist133@gmail.com>
2023-01-17 12:20:18 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -36,11 +36,40 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
+let dot ='10';
+let dash = '11';
+let DECODED_MORSE_TABLE = {};
+
+function decodeMorse(letter){
+    let arr = letter.replaceAll('.',dot)
+    return  arr.replaceAll('-',dash);
+}
+function removeZeros(stroke){
+    return stroke.replaceAll('00','');
+}
+Object.entries(MORSE_TABLE).forEach(([key,value])=>{
+   DECODED_MORSE_TABLE[value] = decodeMorse(key);
+});
+let obj = {};
+Object.entries(DECODED_MORSE_TABLE).forEach(([key,value])=>{
+    obj[value] = key;
+});
 
 function decode(expr) {
-    // write your solution here
+    let output =[];
+    let exprTmp = expr;
+    for(let i = expr.length/10; i>0; i--){
+        if(exprTmp.substring(0,10) === "**********"){
+            output.push(" ");
+            exprTmp = exprTmp.replace(exprTmp.substring(0, 10), '');
+        }
+        else {
+            output.push(obj[removeZeros(exprTmp.substring(0, 10))]);
+            exprTmp = exprTmp.replace(exprTmp.substring(0, 10), '');
+        }
+    }
+    return output.join("");
 }
-
 module.exports = {
     decode
 }
